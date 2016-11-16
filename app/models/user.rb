13bd19bdb2e9f75252b480b51c24a8e2206d6a05
Edward Yu <3dward.yu@gmail.com>
2016-11-16 17:46:11 -0500

    create method in user model to store authentication data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provideruser
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.token = auth.credentials.token
+      user.secret = auth.credentials.token_secret
+      user.omniauth_expries_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
 end

13c460f8724507e2ba7cd5034738204acb3b3ee5
Edward Yu <3dward.yu@gmail.com>
2016-11-16 16:46:06 -0500

    create custom routes for omniauth sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/failure'
-
   get 'photos/index'
-  get '/auth/:fiveHundredPx/callback', to: 'sessions#create'
+  get '/auth/:provider/callback', to: 'sessions#create'
   root 'photos#index'
 
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/failure'
+
+  get '/login', :to => 'sessions#new', :as => :login
+  match '/auth/:provider/callback', :to => 'sessions#create'
+  match '/auth/failure', :to => 'sessions#failure'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

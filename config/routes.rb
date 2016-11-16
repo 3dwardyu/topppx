ee5311abea1ee55e0cdf4d3bc98a70275d8388e8
Edward Yu <3dward.yu@gmail.com>
2016-11-16 17:10:20 -0500

    removed custom routes temporarily

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   root 'photos#index'
 
-  get '/login', :to => 'sessions#new', :as => :login
-  match '/auth/:provider/callback', :to => 'sessions#create'
-  match '/auth/failure', :to => 'sessions#failure'
+  # get '/login', :to => 'sessions#new', :as => :login
+  # match '/auth/failure', :to => 'sessions#failure'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

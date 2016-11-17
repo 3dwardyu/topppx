441a1058cd2237703412296a604fed52ea6ea5b6
Edward Yu <3dward.yu@gmail.com>
2016-11-16 20:38:25 -0500

    create schema and fix syntax for omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.from_omniauth(env["omniauth.auth"])
-    sessions[:user_id] = user.id
+    session[:user_id] = user.id
     redirect_to '/'
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to '/'
+  end
 
   def failure
     redirect_to '/'
